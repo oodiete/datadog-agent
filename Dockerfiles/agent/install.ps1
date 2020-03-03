41f0603e8b55169de7014e3103e3adfc508489ac
db <derek.brown@datadoghq.com>
2020-03-03 14:23:39 -0800

    try msi install waiting for install to complete

--- Dockerfiles/agent/install.ps1
+++ Dockerfiles/agent/install.ps1
@@ -1,2 +1,3 @@
 msiexec /i datadog-agent-7-latest.amd64.msi /qn /L*v install.txt
+start-process msiexec -ArgumentList '/i datadog-agent-7-latest.amd64.msi /qn /L*v install.txt' -Wait
 cat install.txt
